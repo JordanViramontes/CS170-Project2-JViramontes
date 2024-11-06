10a72a3eb8934d6a00a4959a3743b79736ef5234
Jordan Viramontes <jvira007@ucr.edu>
2024-11-06 09:25:22 -0800

    project setup

---
+++ makefile
@@ -0,0 +1,6 @@
+# SOURCES := ./src/main.cpp ./src/graph.cpp ./src/board.cpp ./src/ui.cpp
+
+SOURCES := ./src/main.cpp ./src/graph.cpp
+
+make : $(SOURCES)
+	g++ -g -Wall -o a.exe $(SOURCES)
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+DEVELOPED BY JORDAN VIRAMONTES
+
+Disclaimer:
+ALL code in this project was written by me and is entirely original
+*/
+
+#include <iostream>
+
+#include "classes.h"
+
+using namespace std;
+
+int main() {
+    cout << "hello world\n";
+    return 0;
+}
